cfa6893205394af586b87ec0ae3db497ad3cb10d
Michael Baird <mbaird@users.noreply.github.com>
2017-01-09 10:01:16 +0000

    Fix $PATH for Homebrew

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,5 @@
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.bin:/usr/local/sbin:/usr/local/bin:$PATH"
 
 # load rbenv if available
 if command -v rbenv >/dev/null; then

34c260b4b874765ab953fbd63608ae8242b1103d
Michael Baird <mbaird@users.noreply.github.com>
2024-07-31 17:15:29 +0100

    More vim config cleanup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,8 +12,6 @@ vim.o.shiftround = true       -- Use multiple of shiftwidth when indenting
 vim.o.shiftwidth = 2          -- Use 2 spaces for auto indent
 vim.o.shortmess = 'atIF'      -- Disable intro message, truncate shortmessages
 vim.o.tabstop = 2             -- Number of spaces per tab
-vim.o.textwidth = 100         -- Reasonable wrap width
-vim.o.ttimeoutlen = 10        -- Time to wait for keycode sequences
 vim.o.scrolloff = 3           -- Keep lines of context when scrolling
 vim.o.mouse = ""              -- Disable mouse
 
@@ -39,9 +37,6 @@ vim.keymap.set('n', '<esc><esc>', '<cmd>write<return>')
 -- Close buffer
 vim.keymap.set('n', '<leader>d', '<cmd>bdelete<return>', { silent = true })
 vim.keymap.set('n', '<leader>D', '<cmd>bdelete!<return>', { silent = true })
-
--- Close the current tab
-vim.keymap.set('n', 'gx', '<cmd>tabclose<return>', { silent = true })
 
 -- Exit (non-fzf) Terminal mode with <esc>
 vim.keymap.set(
@@ -69,7 +64,11 @@ vim.api.nvim_create_autocmd('FileType', {
 
 vim.diagnostic.config({
   severity_sort = true,
-  float = { border = 'rounded' },
+  virtual_text = false,
+  float = {
+    header = false,
+    border = 'rounded',
+  },
 })
 
 vim.g.ruby_path = vim.fn.expand('~/.rbenv/shims')
@@ -90,6 +89,7 @@ vim.g.splitjoin_ruby_options_as_arguments = 1
 vim.keymap.set('n', '<leader>t', '<cmd>w <bar> TestNearest<return>')
 vim.keymap.set('n', '<leader>T', '<cmd>w <bar> TestFile<return>')
 vim.keymap.set('n', '<leader>l', '<cmd>w <bar> TestLast<return>')
+vim.keymap.set('n', '<leader>s', '<cmd>w <bar> TestSuite<return>')
 
 -- nvim-treesitter
 require('nvim-treesitter.configs').setup({
@@ -140,14 +140,13 @@ require('lspfuzzy').setup {
   save_last = true,
   fzf_preview = false,
 }
-
 vim.keymap.set('n', '<leader>fr', '<cmd>LspFuzzyLast<return>')
 
 -- nvim-cmp
 local cmp = require('cmp')
 
 local has_words_before = function()
-  unpack = unpack or table.unpack
+  if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then return false end
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
@@ -168,7 +167,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<tab>'] = function(fallback)
       if not cmp.select_next_item() then
-        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+        if has_words_before() then
           cmp.complete()
         else
           fallback()
@@ -178,7 +177,7 @@ cmp.setup({
 
     ['<s-tab>'] = function(fallback)
       if not cmp.select_prev_item() then
-        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+        if has_words_before() then
           cmp.complete()
         else
           fallback()

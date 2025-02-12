a673cc9d9cbd5cc5fafc3e8c0c18e38c1face1ed
Michael Baird <mbaird@users.noreply.github.com>
2025-02-12 15:42:30 +0000

    Consistent borders for vim popups

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,6 +67,18 @@ vim.api.nvim_create_autocmd('FileType', {
   command = 'set filetype=javascriptreact',
 })
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "rounded"
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = "rounded"
+  }
+)
+
 vim.diagnostic.config({
   severity_sort = true,
   virtual_text = false,

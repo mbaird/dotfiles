4a02b6f4b23e6e8cbb25b0818048da269f433094
Michael Baird <mbaird@users.noreply.github.com>
2025-10-10 12:45:17 +0100

    Fix variable setting in fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,35 +1,36 @@
 if status is-interactive
-  set fish_greeting ""
+  set fish_greeting
 
-  set -g fish_autosuggestion_enabled 0
+  set fish_autosuggestion_enabled 0
 
-  set -g __fish_git_prompt_showdirtystate 'yes'
-  set -g __fish_git_prompt_char_cleanstate ""
-  set -g __fish_git_prompt_char_untrackedfiles ""
-  set -g __fish_git_prompt_char_stagedstate ""
-  set -g __fish_git_prompt_char_conflictedstate ""
+  set __fish_git_prompt_showdirtystate 'yes'
+  set __fish_git_prompt_char_cleanstate ""
+  set __fish_git_prompt_char_untrackedfiles ""
+  set __fish_git_prompt_char_stagedstate ""
+  set __fish_git_prompt_char_conflictedstate ""
 end
 
 # homebrew
 fish_add_path /opt/homebrew/bin
-set -gx HOMEBREW_NO_ENV_HINTS 1
+set -x HOMEBREW_NO_ENV_HINTS 1
 
 # mise
-set -gx MISE_DATA_DIR "~/.mise"
+set -x MISE_DATA_DIR $HOME/.mise
 
 # `git trust`
-set PATH ./.git/safe/../../bin $PATH
+set -x PATH ./.git/safe/../../bin $PATH
 
 # 1password ssh
 if test -z "$SSH_TTY"
   set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
 end
 
-set -gx FZF_DEFAULT_COMMAND fd --hidden --type file --color never --exclude .git
-set -gx EDITOR nvim
-set -gx VISUAL $EDITOR
-set -gx PAGER less
-set -gx LESS -F -i -R -S -w -X -z-4
+# fzf
+set -x FZF_DEFAULT_COMMAND fd --hidden --type file --color never --exclude .git
+
+set -x EDITOR nvim
+set -x VISUAL $EDITOR
+set -x LESS -F -i -R -S -w -X -z-4
 
 alias vim nvim
 alias j z
